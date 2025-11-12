ab6b6e0904258eda5caf35e3b30ba3fddbb58956
Saanvi Sharma <saanvi.s019@gmail.com>
2025-11-12 23:42:46 +0530

    feat(cli): add commit and config commands

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+#!/usr/bin/env node
+import { execSync } from "child_process";
 
-console.log("Testing multi-file awareness!");
-console.log("heloo world");
-console.log("Testing level 2");
+const args = process.argv.slice(2);
+const command = args[0];
+
+switch (command) {
+  case "commit":
+    import("./commands/commit.js").then((m) => m.default());
+    break;
+  case "config":
+    import("./commands/config.js").then((m) => m.default());
+    break;
+  default:
+    console.log(`
+CommitAI CLI
+Usage:
+  commitai commit   Generate AI commit messages
+  commitai config   Manage local config
+`);
+}
